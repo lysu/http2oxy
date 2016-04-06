d8cd7052b376606ecd0dc26ad1b762c761c9fe71
lysu <sulifx@gmail.com>
2016-04-06 23:56:18 +0800

    try https.

--- server/app.go
+++ server/app.go
@@ -8,32 +8,69 @@ import (
 	"net/http/httputil"
 	"sync"
 	"time"
+	"io"
+	"io/ioutil"
 )
 
 func main() {
 
 	var srv http.Server
-	srv.Addr = "localhost:4430"
+	srv.Addr = "0.0.0.0:4430"
 	srv.ConnState = idleTimeoutHook()
 
-	proxy := NewReverseProxy()
+	httpProxy := NewReverseProxy()
 
-	http.Handle("/", proxy)
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		isHTTPS := req.TLS != nil
+		if isHTTPS {
+			ServerHTTPS(rw, req)
+		} else {
+			httpProxy.ServeHTTP(rw, req)
+		}
+	})
 
 	http2.ConfigureServer(&srv, &http2.Server{})
 
 	go func() {
-		log.Fatal(srv.ListenAndServeTLS("/Users/didi/key/server.crt", "/Users/didi/key/server.key"))
+		log.Fatal(srv.ListenAndServeTLS("cc.crt", "cc.key"))
 	}()
 
 	select {}
 
 }
 
+func ServerHTTPS(rw http.ResponseWriter, req *http.Request) {
+	pr, pw := io.Pipe()
+	req, err := http.NewRequest(req.Method, req.URL.String(), ioutil.NopCloser(pr))
+	if err != nil {
+		panic(err)
+	}
+	req.URL.Scheme = "https"
+	req.URL.Host = req.Host
+	req.URL.Path = req.URL.Path
+	if req.URL.RawQuery != "" {
+		req.URL.RawPath = req.URL.RawQuery
+	}
+	go func() {
+		_, err := io.Copy(pw, req.Body)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(rw, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func NewReverseProxy() *httputil.ReverseProxy {
 	director := func(req *http.Request) {
-		req.URL.Scheme = req.URL.Scheme
-		req.URL.Host = req.URL.Host
+		req.URL.Scheme = "http"
+		req.URL.Host = req.Host
 		req.URL.Path = req.URL.Path
 		if req.URL.RawQuery != "" {
 			req.URL.RawPath = req.URL.RawQuery
