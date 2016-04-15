9f1b283af5d2e6daefe6e6d096be8f4e41789ace
lysu <sulifx@gmail.com>
2016-04-15 23:30:57 +0800

    manual pipe

--- app.go
+++ app.go
@@ -34,27 +34,93 @@ func main() {
 }
 
 func ServerHTTPS(rw http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Connection", "close")
 	remoteConn, err := net.Dial("tcp", req.Host)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Connected %s\n", req.Host)
-	go func() {
-		remoteConn.SetDeadline(time.Now().Add(200 * time.Millisecond))
-		_, err := io.Copy(remoteConn, req.Body)
-		if err != nil {
-			panic(err)
-		}
-	}()
-	go func() {
 
-		remoteConn.SetDeadline(time.Now().Add(200 * time.Millisecond))
-		_, err = io.Copy(rw, remoteConn)
-		if err != nil {
-			panic(err)
+	go Pipe(remoteConn, &clientConn{
+		Reader: req.Body,
+		Writer: rw,
+	})
+	rw.WriteHeader(http.StatusOK)
+	fmt.Println(remoteConn)
+
+	//a, err := ioutil.ReadAll(req.Body)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//log.Printf("----===%s\n", string(a))
+}
+
+type x interface {
+	Write(b []byte) (n int, err error)
+	Read(b []byte) (n int, err error)
+	SetDeadline(t time.Time) error
+}
+
+type clientConn struct {
+	io.Reader
+	io.Writer
+}
+
+func (c *clientConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func Pipe(conn1 x, conn2 x) {
+	chan1 := makeConnChan(conn1, 1)
+	chan2 := makeConnChan(conn2, 2)
+
+	for {
+		select {
+		case b1 := <-chan1:
+			if b1 == nil {
+				return
+			} else {
+				conn2.Write(b1)
+			}
+		case b2 := <-chan2:
+			if b2 == nil {
+				return
+			} else {
+				conn1.Write(b2)
+			}
+		}
+	}
+}
+
+func makeConnChan(conn x, num int) chan []byte {
+	c := make(chan []byte)
+
+	go func() {
+		b := make([]byte, 1024)
+
+		for {
+			conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
+			n, err := conn.Read(b)
+
+			if n > 0 {
+				res := make([]byte, n)
+				copy(res, b[:n])
+				c <- res
+			}
+
+			if err != nil {
+				c <- nil
+				if err != io.EOF {
+					log.Printf("%d Piping error %s", num, err)
+				} else {
+					log.Printf("reading finished")
+				}
+			}
 		}
 	}()
-	rw.Write([]byte("HTTP/1.0 200 Connection established\r\n\r\n"))
+
+	return c
 }
 
 func NewReverseProxy() *httputil.ReverseProxy {
